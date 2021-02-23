f4f1bca6462880274cd439b7e407289532121acd
Emanuel Guerra <49962003+EmanuelGuerra@users.noreply.github.com>
2021-02-22 22:12:44 -0600

    Modificacion a Login
    He modificado algunos detalles esteticos de login

--- mantenimiento/login.php
+++ mantenimiento/login.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,19 +8,20 @@
 	<link href="librerias/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-	<div class="container" >
+	<div class="container text-center" >
         <div class="row text-center login-page">
-            <div class="col-md-12 login-form">
-			<div class="col-md-12">
-				<h2>Login</h2>
+			<div class="col-md-12 login-div">
+            	<div class="panel panel-default">
+            		<div class="panel-heading">
+					<strong class="">Login</strong>
+				</div>
 			</div>
-		</div>
 		<form action="validacion.php" method="post"> 			
 		</div>
         <br>
 		<div class="row">
 		   <div class="col-md-12 login-from-row">
-		      <input name="nombre" type="text" placeholder="Nombre" required/>
+		      <input name="nombre" type="text" placeholder="Usuario" required/>
 		   </div>
 		</div>
         <br>
@@ -34,6 +34,12 @@
 		<div class="row">
 		   <div class="col-md-12 login-from-row">
 		      <button class="btn btn-info">Entrar</button>
+		   </div>
+		</div>
+		<br>
+		<div class="row">
+		   <div class="col-md-12 login-from-row">
+		      <button class="btn btn-success">Registrarse</button>
 		   </div>
 		</div>
 	    </form>
