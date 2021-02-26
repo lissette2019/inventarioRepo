41517b0b44d32535dc8a5dafbb21cef27e950272
EmaunelGuerra <emanuelguerra9610@gmail.com>
2021-02-25 21:41:10 -0600

    Se agrego algunos cambios en conexiones de login y para registro

--- mantenimiento/login.php
+++ mantenimiento/login.php
@@ -1,44 +1,50 @@
-<!DOCTYPE html>
-<html lang="es">
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>CRUD</title>
-	<link href="librerias/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
-	<div class="container" >
-        <div class="row text-center login-page">
-            <div class="col-md-12 login-form">
-			<div class="col-md-12">
-				<h2>Login</h2>
-			</div>
-		</div>
-		<form action="validacion.php" method="post"> 			
-		</div>
-        <br>
-		<div class="row">
-		   <div class="col-md-12 login-from-row">
-		      <input name="nombre" type="text" placeholder="Nombre" required/>
-		   </div>
-		</div>
-        <br>
-		<div class="row">
-		   <div class="col-md-12 login-from-row">
-		      <input name="password" type="password" placeholder="Contraseña" required/>
-		   </div>
-		</div>
-        <br>
-		<div class="row">
-		   <div class="col-md-12 login-from-row">
-		      <button class="btn btn-info">Entrar</button>
-		   </div>
-		</div>
-	    </form>
-	</div>
-</body>
-    <script src="librerias/js/jquery-1.11.3.min.js"></script>
-    <script src="librerias/js/bootstrap.min.js"></script>
-</html>
+<! DOCTYPE html>
+< html  lang = " es " >
+< cabeza >
+    < meta  charset = " utf-8 " >
+    < meta  http-equiv = " X-UA-Compatible "  content = " IE = edge " >
+    < meta  name = " viewport "  content = " width = device-width, initial-scale = 1 " >
+	< título > CRUD </ título >
+	< link  href = " librerias / css / bootstrap.min.css "  rel = " hoja de estilo " >
+</ cabeza >
+< cuerpo >
+	< div  class = " contenedor text-center " >
+        < div  class = " row text-center login-page " >
+			< div  class = " col-md-12 login-div " >
+            	< div  class = " panel panel-default " >
+            		< div  class = " panel-header " >
+					< strong  class = " " > Iniciar sesión </ strong >
+				</ div >
+			</ div >
+		< form  action = " validacion.php "  method = " post " > 			
+		</ div >
+        < Br >
+		< div  class = " fila " >
+		   < div  class = " col-md-12 login-from-row " >
+		      < input  name = " nombre "  type = " text "  placeholder = " Usuario "  required />
+		   </ div >
+		</ div >
+        < Br >
+		< div  class = " fila " >
+		   < div  class = " col-md-12 login-from-row " >
+		      < input  name = " password "  type = " password "  placeholder = " Contraseña "  obligatorio />
+		   </ div >
+		</ div >
+        < Br >
+		< div  class = " fila " >
+		   < div  class = " col-md-12 login-from-row " >
+		      < button  class = " btn btn-info " > Entrar </ button >
+		   </ div >
+		</ div >
+		< Br >
+		< div  class = " fila " >
+		   < div  class = " col-md-12 login-from-row " >
+		      < button  class = " btn btn-success " > Registrarse </ button >
+		   </ div >
+		</ div >
+	    </ formulario >
+	</ div >
+</ cuerpo >
+    < script  src = " librerias / js / jquery-1.11.3.min.js " > </ script >
+    < script  src = " librerias / js / bootstrap.min.js " > </ script >
+</ html >
