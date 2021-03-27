c1b15b40aaa4958b7d419549ef344d1b30eb752e
claudia.2015lissette@gmail.com <50220959+lissette2019@users.noreply.github.com>
2021-03-27 17:14:25 -0600

    Update index.php

--- index.php
+++ index.php
@@ -4,21 +4,48 @@ session_start();
 $usuario = $_SESSION['usuario'];
 
 ?>
-
 <!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bienvenido</title>
-</head>
-<body>
-<?php 
-    echo 'Bienvenido Usuario' .$usuario; 
-?>
+        <html>
+        <head>
+            <meta charset="utf-8">
+            <title>Bienvenido al Inventario</title>
+            <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+            <link rel="stylesheet" href="assets/css/style.css">
+        </head>
+        <body>
 
-    
-    
-</body>
-</html>
+        <?php if(!empty($user)): ?>
+            <br>Bienvenido <?= $user['email']; ?>
+            <br>Ha iniciado sesión correctamente
+            <a href="CerrarSesion.php">
+               CerrarSesion
+            </a>
+            <?php else: ?>
+            <h1>Por favor ingresa o regístrate</h1>
+
+            <a href="Login.php">Login</a> or
+            <a href="Registro.php">Registrate</a>
+            <?php endif; ?>
+
+        <header id="main-header">
+                
+                <a id="logo-header" href="/assets/Imagenes/LOGO INVENTARIO.png">
+                </a> <!-- / #logo-header -->
+
+                <nav>
+                    
+                        <a href="Login.php">Login,</a>
+                        <a href="Registro.php">Registro,</a>
+                        <a href="CerrarSesion.php">Salir.</a>
+                
+                </nav><!-- / nav -->
+
+            </header><!-- / #main-header -->
+
+            <h1>Bienvenido al Inventario Digital</h1>
+            <h3>Hospital Militar Central</h3>
+            <?php 
+                echo 'Bienvenido Usuario' .$usuario; 
+            ?>
+        </body>
+ </html>
