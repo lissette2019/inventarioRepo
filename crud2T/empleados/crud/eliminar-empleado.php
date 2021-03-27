1e33775ca71afcc8e7e26849e803cd666b28b551
EmaunelGuerra <emanuelguerra9610@gmail.com>
2021-03-26 23:30:33 -0600

    Comentarios para desarrollo de soft delete

--- crud2T/empleados/crud/eliminar-empleado.php
+++ crud2T/empleados/crud/eliminar-empleado.php
@@ -2,7 +2,11 @@
 	include("../../includes/conexion.php");
 
 	// obtener inputs entre las etiquetas form
-	$empleadoId = $_REQUEST["empleadoId"];
-	$delete = mysqli_query($conexion,"DELETE FROM empleados WHERE empleadoId='$empleadoId'");
-	header("Location: ../empleados.php");
+	//$empleadoId = $_REQUEST["empleadoId"];
+	//$delete = mysqli_query($conexion,"DELETE FROM empleados WHERE empleadoId='$empleadoId'");
+	//header("Location: ../empleados.php");
+	<br>Aqui se cometo el Delete para poder crear softdelete<br>
+	<br>Aqui se cometo el Delete para poder crear softdelete<br>
+	<br>Aqui se cometo el Delete para poder crear softdelete<br>
+	<br>Aqui se cometo el Delete para poder crear softdelete<br>
 ?>
